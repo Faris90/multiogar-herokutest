39361476b641cce4a01b1b8bcd5a1439a64579a5
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-01-21 17:09:49 -0600

    Add files via upload

---
+++ entity/EjectedMass.js
@@ -0,0 +1,33 @@
+﻿var Cell = require('./Cell');
+
+function EjectedMass() {
+    Cell.apply(this, Array.prototype.slice.call(arguments));
+    
+    this.cellType = 3;
+}
+
+module.exports = EjectedMass;
+EjectedMass.prototype = new Cell();
+
+// Main Functions
+
+EjectedMass.prototype.canEat = function (cell) {
+    // cannot eat if virusMaxAmount is reached
+    if (this.gameServer.nodesVirus.length < this.gameServer.config.virusMaxAmount)
+        return cell.cellType == 0 // virus can eat ejected mass only
+         return cell.cellType == 2
+	     return cell.cellType == 3;
+	};
+	
+EjectedMass.prototype.onAdd = function (gameServer) {
+    // Add to list of ejected mass
+    gameServer.nodesEjected.push(this);
+};
+
+EjectedMass.prototype.onRemove = function (gameServer) {
+    // Remove from list of ejected mass
+    var index = gameServer.nodesEjected.indexOf(this);
+    if (index != -1) {
+        gameServer.nodesEjected.splice(index, 1);
+    }
+};
